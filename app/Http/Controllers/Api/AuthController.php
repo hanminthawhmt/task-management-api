64f1d6cbd180e0cbfc1027dc88669b8841f9f9e8
Han Min Thaw <hanminthaw15@gmail.com>
2026-02-27 16:10:22 +0700

    auth set up with Laravel Sanctum

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,45 @@
+<?php
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\CreateUserRequest;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function register(CreateUserRequest $request)
+    {
+        $data = $request->validated();
+
+        $user = User::create($data);
+
+        $token = $user->createToken('api-token')->plainTextToken;
+
+        return response()->json([
+            'user'  => new UserResource($user),
+            'token' => $token,
+        ]);
+    }
+
+    public function login(Request $request)
+    {
+        if (! Auth::attempt($request->only('email', 'password'))) {
+            return response()->json([
+                'message' => 'Invalid Credentials',
+            ]);
+        }
+
+        $user  = Auth::user();
+        $token = $user->createToken('api-token')->plainTextToken;
+
+        return response()->json([
+            'user'  => new UserResource($user),
+            'token' => $token,
+        ]);
+
+    }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -7,6 +8,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+
+});
 
 Route::patch('tasks/{task}/complete', [TaskController::class, 'markAsComplete']);
 Route::patch('tasks/{id}/update', [TaskController::class, 'updateStatus']);
